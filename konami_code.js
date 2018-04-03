01069bf4088b44c3223d962ed2c7da72cb16b45e
Christopher <christopherccm@msn.com>
2018-04-03 17:26:00 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,11 +2,6 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0;
 
-function onKeyDownHandler(e) {
-
-  console.log(e);
-}
-
 function init() {
   document.body.addEventListener('keydown', event => {
     const e = parseInt(event.detail || event.which);
